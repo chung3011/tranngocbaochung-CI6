810b9c14d3858b796a928884f2d93683f7b3e7ab
chung3011 <chung.hitc97@gmail.com>
2018-05-23 20:25:21 +0700

    ccc

--- Module2/src/GameCanvas.java
+++ Module2/src/GameCanvas.java
@@ -9,8 +9,8 @@ import java.util.Random;
 public class GameCanvas extends JPanel {
 
     BufferedImage playerImage;
-    public int positionXPlayer=512;
-    public int positionYPlayer=300;
+    public int positionXPlayer = 512;
+    public int positionYPlayer = 300;
 
     BufferedImage starImage;
 //    public int positionXStar=600;
@@ -21,52 +21,92 @@ public class GameCanvas extends JPanel {
 //    public int positionYEnemy=500;
     public Point[] enemiesList = new Point[100];
 
+    BufferedImage backBuffered;
+    Graphics graphics;
 
     public GameCanvas() {
 
         Random rand = new Random();
-        for (int i = 0; i < this.enemiesList.length; i++){
-            while (true){
+        for (int i = 0; i < this.enemiesList.length; i++) {
+            while (true) {
                 int positionXEnemy = rand.nextInt(1024);
                 int positionYEnemy = rand.nextInt(600);
-                if (positionXEnemy!=512 && positionYEnemy!=300)
-                    this.enemiesList[i] = new Point(positionXEnemy,positionYEnemy);
-                    break;
+                if (positionXEnemy != 512 && positionYEnemy != 300)
+                    this.enemiesList[i] = new Point(positionXEnemy, positionYEnemy);
+                break;
             }
         }
 
-        for(int i = 0; i< this.starsList.length; i++){
+        for (int i = 0; i < this.starsList.length; i++) {
             int positionXStar = rand.nextInt(1024);
             int positionYStar = rand.nextInt(600);
-            this.starsList[i] = new Point(positionXStar,positionYStar);
+            this.starsList[i] = new Point(positionXStar, positionYStar);
         }
 
         this.setSize(1024, 600);
 
         //load image
-        try {
-            this.starImage = ImageIO.read(new File("resources/resources/images/star.png"));
-            this.enemyImage = ImageIO.read(new File("resources/resources/images/circle.png"));
-            this.playerImage = ImageIO.read(new File("resources/resources/images/circle.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            this.starImage = ImageIO.read(new File("resources/resources/images/star.png"));
+//            this.enemyImage = ImageIO.read(new File("resources/resources/images/circle.png"));
+//            this.playerImage = ImageIO.read(new File("resources/resources/images/circle.png"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        this.setupBackBuffered();
+        this.setupCharacter();
         this.setVisible(true);
+    }
+
+    private void setupBackBuffered(){
+        this.backBuffered = new BufferedImage(1024, 600, BufferedImage.TYPE_4BYTE_ABGR);
+        this.graphics = this.backBuffered.getGraphics();
+    }
+
+    private void setupCharacter(){
+        this.starImage = this.loadImage("resources/resources/images/star.png");
+        this.enemyImage = this.loadImage("resources/resources/images/circle.png");
+        this.playerImage = this.loadImage("resources/resources/images/circle.png");
     }
 
     @Override
     protected void paintComponent(Graphics g) {
+        g.drawImage(this.backBuffered, 0, 0, null);
+    }
 
-        g.setColor(Color.BLACK);
-        g.fillRect(0,0,1024,600);
-//        g.drawImage(this.starImage,this.positionXStar,this.positionYStar,10,10,null);
-//        g.drawImage(this.enemyImage,this.positionXEnemy,this.positionYEnemy,20,20,null);
-        g.drawImage(this.playerImage,this.positionXPlayer,this.positionYPlayer,20,20,null);
-        for (int i = 0; i <this.enemiesList.length; i++){
-            g.drawImage(this.enemyImage, this.enemiesList[i].x, this.enemiesList[i].y,10,10,null);
+    public void renderAll() {
+        this.renderBackground();
+        this.graphics.drawImage(this.playerImage, this.positionXPlayer, this.positionYPlayer, 20, 20, null);
+        for (int i = 0; i < this.enemiesList.length; i++) {
+            this.graphics.drawImage(this.enemyImage, this.enemiesList[i].x, this.enemiesList[i].y, 10, 10, null);
         }
-        for (int i = 0; i <this.starsList.length; i++){
-            g.drawImage(this.starImage, this.starsList[i].x,this.starsList[i].y, 5, 5, null);
+        for (int i = 0; i < this.starsList.length; i++) {
+            this.graphics.drawImage(this.starImage, this.starsList[i].x, this.starsList[i].y, 5, 5, null);
+        }
+        this.repaint();
+    }
+
+    private void renderBackground(){
+        this.graphics.setColor(Color.BLACK);
+        this.graphics.fillRect(0, 0, 1024, 600);
+    }
+
+    public void runAll(){
+        for (int i = 0; i < this.starsList.length; i++) {
+            this.starsList[i].x = (this.starsList[i].x + 3) % 1024;
+        }
+        for (int i = 0; i < this.enemiesList.length; i++) {
+            this.enemiesList[i].y = (this.enemiesList[i].y + 3) % 600;
+        }
+    }
+
+    private BufferedImage loadImage(String path) {
+        try {
+            return ImageIO.read(new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
--- Module2/src/GameWindow.java
+++ Module2/src/GameWindow.java
@@ -1,67 +1,85 @@
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 
 public class GameWindow extends JFrame {
     GameCanvas gameCanvas;
-    long lastTime=0;
-    public GameWindow(){
-        this.setSize(1024,600);
+    long lastTime = 0;
+
+    public GameWindow() {
+        this.setSize(1024, 600);
+        this.setupGameCanvas();
+        this.event();
+        this.setVisible(true); // cho phép cửa sổ hiển thị lên
+    }
+
+    private void setupGameCanvas() {
         this.gameCanvas = new GameCanvas();
         this.add(this.gameCanvas);
+    }
+
+    private void event() {
+        this.keyboardEvent();
+        this.windowEvent();
+    }
+
+    private void keyboardEvent() {
         this.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
-
             }
 
             @Override
             public void keyPressed(KeyEvent e) {
-                if(e.getKeyCode()== KeyEvent.VK_LEFT){
-                    if (gameCanvas.positionXPlayer>0)
-                    gameCanvas.positionXPlayer=(gameCanvas.positionXPlayer-10)%1024;
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    if (gameCanvas.positionXPlayer > 0)
+                        gameCanvas.positionXPlayer = (gameCanvas.positionXPlayer - 10) % 1024;
                     else
-                        gameCanvas.positionXPlayer=1024;
+                        gameCanvas.positionXPlayer = 1024;
                 }
-                if(e.getKeyCode()== KeyEvent.VK_RIGHT){
-                    gameCanvas.positionXPlayer=(gameCanvas.positionXPlayer+10)%1024;
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    gameCanvas.positionXPlayer = (gameCanvas.positionXPlayer + 10) % 1024;
                 }
-                if(e.getKeyCode()== KeyEvent.VK_UP){
-                    if (gameCanvas.positionYPlayer>0)
-                        gameCanvas.positionYPlayer=(gameCanvas.positionYPlayer-10)%600;
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    if (gameCanvas.positionYPlayer > 0)
+                        gameCanvas.positionYPlayer = (gameCanvas.positionYPlayer - 10) % 600;
                     else
-                        gameCanvas.positionYPlayer=600;
+                        gameCanvas.positionYPlayer = 600;
                 }
-                if(e.getKeyCode()== KeyEvent.VK_DOWN){
-                    gameCanvas.positionYPlayer=(gameCanvas.positionYPlayer+10)%600;
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    gameCanvas.positionYPlayer = (gameCanvas.positionYPlayer + 10) % 600;
                 }
             }
 
             @Override
             public void keyReleased(KeyEvent e) {
-
             }
         });
-        this.setVisible(true); // cho phép cửa sổ hiển thị lên
-        }
-        public void gameLoop(){
-        while (true){
+    }
+
+    private void windowEvent() {
+        this.addWindowFocusListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(1);
+            }
+        });
+    }
+
+    public void gameLoop() {
+        while (true) {
             long currentTime = System.nanoTime();
-            if (currentTime - lastTime >= 17_000_000)
-            {
+            if (currentTime - lastTime >= 17_000_000) {
 //                this.gameCanvas.positionXStar=(this.gameCanvas.positionXStar+3)%1024;
 //                this.gameCanvas.positionYEnemy=(this.gameCanvas.positionYEnemy+3)%600;
-                for(int i=0; i<this.gameCanvas.starsList.length;i++){
-                    this.gameCanvas.starsList[i].x =(this.gameCanvas.starsList[i].x+3)%1024;
-                }
-                for(int i=0; i<this.gameCanvas.enemiesList.length;i++){
-                    this.gameCanvas.enemiesList[i].y =(this.gameCanvas.enemiesList[i].y+3)%600;
-                }
-                this.gameCanvas.repaint();
+                this.gameCanvas.runAll();
+                this.gameCanvas.renderAll();
                 this.lastTime = currentTime;
             }
 
         }
-        }
+    }
 }
