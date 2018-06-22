4c3ee14eab784821fa72db519c3f6fcf7326f3f7
chung3011 <chung.hitc97@gmail.com>
2018-06-22 20:06:40 +0700

    chung

---
+++ CI-Project/src/GameWindow.java
@@ -0,0 +1,75 @@
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+
+public class  GameWindow extends JFrame {
+
+    GameCanvas gameCanvas;
+    long lastTime = 0;
+
+    public GameWindow() {
+        this.setSize(1024, 600);
+        this.gameCanvas = new GameCanvas();
+        this.add(this.gameCanvas);
+        this.event();
+        this.setVisible(true);
+    }
+
+    private void event() {
+        this.keyboardEvent();
+        this.windowEvent();
+    }
+
+    private void keyboardEvent() {
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+                    gameCanvas.isCatching = true;
+                    gameCanvas.ropeDirection.set(gameCanvas.ballPosition);
+                    gameCanvas.movingDirection.set(gameCanvas.ballPosition.subtractBy(gameCanvas.anchorPosition).normalize().multiply(3));
+                    gameCanvas.isDropping = true;
+                }
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+
+                }
+
+            }
+        });
+    }
+
+
+    private void windowEvent() {
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(1);
+            }
+        });
+    }
+
+    public void gameLoop() {
+        while (true) {
+            long currentTime = System.nanoTime();
+
+            if (currentTime - this.lastTime >= 17_000_000) {
+                this.gameCanvas.runAll();
+                this.gameCanvas.renderAll();
+                this.lastTime = currentTime;
+            }
+        }
+
+    }
+}
